a1b46325aba9a7811f4b7e84b3c998033bab2365
T-Dex <dex.m.mills@gmail.com>
2017-03-28 18:28:53 -0600

    feat (main) Add new JS and style CSS

--- scripts/chucknorrisapi.js
+++ scripts/chucknorrisapi.js
@@ -6,9 +6,9 @@ window.onload = function gettingFunnyWithChuck() {
    return value;
 };
 
-let jokeNum = buwHaHaHa();
-
-
+let jokeNum =  buwHaHaHa();
+let hardyHar=[];
+var btn = document.querySelector("button");
 
   const randomJoke = 'id/'
   const url = 'http://api.icndb.com/jokes'+ randomJoke
@@ -18,9 +18,11 @@ let jokeNum = buwHaHaHa();
   }).done(function(data){
     console.log('the returned data is:', data)
 
-    if ( randomJoke ==='id/'){
-
+    btn.addEventListener('click', function(){
       const comedy = data.value;
+      hardyHar = comedy.filter(function(element, index){
+        return element.joke})
+
       const idkLength = comedy.length;
       const jokes= [];
 
@@ -28,16 +30,16 @@ let jokeNum = buwHaHaHa();
       for (let i = 0; i < idkLength; i++){
       jokes.push(comedy[i])
       }
+
+      document.getElementById('input').innerHTML= jokes[jokeNum].joke;
+    // document.getElementById('input').innerHTML= jokes[jokeNum];
     console.log(jokes[jokeNum]);
     return jokes[jokeNum];
-    }
+      // document.getElementById('input').innerHTML= hardyHar.joke;
 
-let refresh = document.querySelector('p.joke');
-      let btn = document.querySelector("button.buttons"); btn.addEventListener('click', function(){
-        letter = refresh.value;
-        console.log(letter);
-        return letter;
-      });
 
- })
+
+  })
+  })
+
 }
