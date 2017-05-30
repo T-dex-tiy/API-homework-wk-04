e961fc15587deee7aef24a9190fb9fe679465be8
T-Dex <dex.m.mills@gmail.com>
2017-05-30 17:19:45 -0600

    Update user input display

--- scripts/chucknorrisapi.js
+++ scripts/chucknorrisapi.js
@@ -9,35 +9,39 @@ let jokeNum =  buwHaHaHa();
 let hardyHar=[];
 var btn = document.querySelector("button");
 
+
   const randomJoke = 'id/'
-  const url = '//api.icndb.com/jokes'+ randomJoke
+  const url = 'http://api.icndb.com/jokes'+ randomJoke
 
   $.ajax({
     url: url
   }).done(function(data){
-    console.log('the returned data is:', data)
-
+    // console.log('the returned data is:', data)
     btn.addEventListener('click', function(){
       const comedy = data.value;
+      console.log("comedy data:", comedy);
       hardyHar = comedy.filter(function(element, index){
         return element.joke})
-
       const idkLength = comedy.length;
       const jokes= [];
 
 
+
       for (let i = 0; i < idkLength; i++){
       jokes.push(comedy[i])
-
       }
-      let humorless= jokes[jokeNum].joke;
+      console.log("jokes obj cat;", jokes[jokeNum].categories.length);
+      console.log("jokes obj;", jokes[jokeNum]);
+
+      let humorless = jokes[jokeNum].joke;
+      console.log("humorless;", humorless);
 
       function moreHardyHar(humorless){
       document.getElementById('input').innerHTML= humorless;
       }
 
 
-    console.log(jokes[jokeNum]);
+
     moreHardyHar(humorless);
     buwHaHaHa();
     gettingFunnyWithChuck();
