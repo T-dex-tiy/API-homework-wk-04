6d17fbd349b67fd199e70b34872bac63489fcc9a
T-Dex <dex.m.mills@gmail.com>
2018-09-06 03:46:13 -0600

    Refactor JS

--- scripts/chucknorrisapi.js
+++ scripts/chucknorrisapi.js
@@ -10,6 +10,7 @@ var btn = document.querySelector("button");
 var radio=document.getElementById('SFW')
 let jokes= [];
 let testJokes=[];
+let newHumor;
 let humorless=[];
 
 
@@ -21,45 +22,36 @@ let humorless=[];
     url: url
   }).done(function(data){
     btn.addEventListener('click', function(){
-      console.log(humorless, "enter");
       
       let comedy = data.value;
       testJokes={...comedy}
-      let newHumor= Object.keys(testJokes).filter(key=>{  
-        if(testJokes[key].id==jokeNum){
-          console.log(testJokes[key]);
-          
+      newHumor= Object.keys(testJokes).filter(key=>{  
+        if(testJokes[key].id==jokeNum){ 
           return jokes.push(testJokes[key])
         }
       })
-      console.log(humorless[newHumor], "before");
-
-      function moreHardyHar(humorless){
-        humorless = testJokes[newHumor].joke
-        let explict=testJokes[newHumor].categories
-        if(radio.checked==true&&explict=="explicit"){
-          // document.getElementById('input').innerHTML= humorless
-          alert("This one is too much for work, Cindy in HR would not be happy. We are going to send you another one that Cindy will be okay with")
-          }else{
-            screen=document.getElementById('input').innerHTML= humorless
-          }
-
-      }
-
-
-
     moreHardyHar(humorless);
     buwHaHaHa();
     gettingFunnyWithChuck();
     jokes=[]
     comedy=[]
     humorless=''
-    explict=''
-    console.log(screen);
-    console.log(humorless);
-    
+    explict=''   
     return
   })
   })
 
+  function moreHardyHar(humorless){
+    humorless = testJokes[newHumor].joke
+    let explict=testJokes[newHumor].categories
+    if(radio.checked==true&&explict=="explicit"){
+      alert("This joke is too much for work, Cindy in HR would not be happy. We are going to send you another one that Cindy will be okay with")
+      }else{
+        screen=document.getElementById('input').innerHTML= humorless
+      }
+  
+  }
 }
+
+
+
